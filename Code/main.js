995c2455e4b7118bb5e89d72ab0e0334d85b5090
Lazykangeroo <fvantonder00@gmail.com>
2025-02-15 17:14:00 +0200

    Action Btns setup
    Dont do anything yet. Merely in setup phase

--- Code/main.js
+++ Code/main.js
@@ -1,3 +1,6 @@
+/*
+  Sorting
+*/
 // Select all radio buttons
 const radios = document.querySelectorAll('input[type="radio"]');
 
@@ -9,4 +12,21 @@ radios.forEach(radio => {
       console.log(`Selected option: ${selectedRadio.id}`);
     }
   });
+});
+
+/*
+  Actions
+*/
+const add_player = document.querySelector('input[type="button"]#add-player');
+const add_game = document.querySelector('input[type="button"]#add-game');
+const home = document.querySelector('input[type="button"]#home');
+
+add_game.addEventListener('click', () => {
+  console.log(`Add Game`);
+});
+add_player.addEventListener('click', () => {
+  console.log(`Add Player`);
+});
+home.addEventListener('click', () => {
+  console.log(`Home`);
 });

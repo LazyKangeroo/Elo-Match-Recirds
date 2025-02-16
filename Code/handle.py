2a720d5d8377e80aeab22afcdf76a9fb54eb44c6
Lazykangeroo <fvantonder00@gmail.com>
2025-02-16 12:50:19 +0200

    Different Frames, New Player
    I created frames to show and hide which will act as the different ''screens'.
    Otherwise I made the new player frame/screen and functionality so it will be recorded - but not yet saved to json file (still have to do that next)

---
+++ Code/handle.py
@@ -0,0 +1,18 @@
+class New:
+    def __init__(self):
+        pass
+
+    def player(self, name, surname, grade):
+        player = {
+            "name" : name,
+            "surname" : surname,
+            "grade" : grade
+        }
+        print(player)
+
+class Sort:
+    def __init__(self):
+        pass
+
+def screenChanges(i):
+    return i + 1
--- Code/main.py
+++ Code/main.py
@@ -1,27 +1,105 @@
 from tkinter import *
+from handle import *
 
-#root window
-root = Tk()
-root.title('Chess Team Rankings')
-root.geometry('950x700')
+# Handle modules
+new = New()
+sort = Sort()
 
-# adding lbl
-lbl = Label(root,text="Hello")
-lbl.grid()
+count = 0
 
-# function to display text when
-# button is clicked
-def clicked():
-    lbl.configure(text = "I just got clicked")
+#main window
+win = Tk()
 
-# button widget with red color text
-# inside
-btn = Button(root, text = "Click me" ,fg = "red", command=clicked)
-# set Button grid
-btn.grid(column=1, row=0)
+# Show Hide
+def show_hide(widget1,widget2,widget3,widget4):
+    widget1.pack()
+    widget2.pack_forget()
+    widget3.pack_forget()
+    widget4.pack_forget()
+    global count
+    count += 1
 
+# details
+win.title('Chess Team Rankings')
+win.geometry('950x700')
+
+icon = PhotoImage(file='./icons/chess-board.png')
+win.iconphoto(True,icon)
 
 # all widgets will be here
+# Head frame
+frame_head = Frame(win)
+frame_head.pack(side=TOP)
+lblHead = Label(frame_head,text='Chess Game Records',font=('Arial',20,'bold'),fg='#5c5fff')
+lblHead.pack()
 
-# run main root
-root.mainloop()
+# Home frame
+frame_home = Frame(win)
+frame_home.pack()
+
+# New Game frame
+frame_newgame = Frame(win)
+frame_newgame.pack()
+
+# New player frame
+frame_newplayer = Frame(win)
+frame_newplayer.pack()
+
+innerFrame_new_name = Frame(frame_newplayer,pady=20)
+innerFrame_new_name.pack()
+innerFrame_new_surname = Frame(frame_newplayer,pady=20)
+innerFrame_new_surname.pack()
+innerFrame_new_grade = Frame(frame_newplayer,pady=20)
+innerFrame_new_grade.pack()
+
+    # lbl
+lblName = Label(innerFrame_new_name,font=('Arial',13),text='First Name',padx=10)
+lblSurname = Label(innerFrame_new_surname,font=('Arial',13),text='Last Name',padx=10)
+lblGrade = Label(innerFrame_new_grade,font=('Arial',13),text='Grade',padx=12)
+    # Textbox
+entryName = Entry(innerFrame_new_name,font=('Arial',13),width=20)
+entrySurname = Entry(innerFrame_new_surname,font=('Arial',13),width=20)
+entryGrade = Entry(innerFrame_new_grade,font=('Arial',13),width=20)
+    # btn
+btnSubmitNewPlayer = Button(frame_newplayer,font=('Arial',15,'bold'),text='Submit',activebackground='#111d2e',activeforeground='#5b8fd9', bg='#5b8fd9', fg='#111d2e', command=lambda : new.player(entryName.get(),entrySurname.get(),entryGrade.get()))
+    # Display
+lblName.pack(side=LEFT)
+lblSurname.pack(side=LEFT)
+lblGrade.pack(side=LEFT)
+entryName.pack(side=RIGHT)
+entrySurname.pack(side=RIGHT)
+entryGrade.pack(side=RIGHT)
+btnSubmitNewPlayer.pack(side=BOTTOM)
+
+# Player pf frame
+frame_profile = Frame(win)
+frame_profile.pack()
+
+# menu
+menubar = Menu(win)
+win.config(menu=menubar)
+
+# sort players by
+sortMenu = Menu(menubar,tearoff=0, font=('Arial',10))
+menubar.add_cascade(label='Sort',menu=sortMenu)
+sortMenu.add_command(label='Name')
+sortMenu.add_command(label='Grade')
+sortMenu.add_command(label='Elo')
+sortMenu.add_separator()
+sortMenu.add_command(label='Wins')
+sortMenu.add_command(label='Lost')
+sortMenu.add_command(label='Draws')
+
+# actions
+viewMenu = Menu(menubar,tearoff=0,font=('Arial',10))
+menubar.add_cascade(label='View',menu=viewMenu)
+viewMenu.add_command(label='New Player',command=lambda : show_hide(frame_newplayer,frame_newgame,frame_home,frame_profile))
+viewMenu.add_command(label='New Game',command=lambda : show_hide(frame_newgame,frame_newplayer,frame_home,frame_profile))
+viewMenu.add_separator()
+viewMenu.add_command(label='Home',command=lambda : show_hide(frame_home,frame_newgame,frame_newplayer,frame_profile))
+
+if count < 1:
+    show_hide(frame_home,frame_newgame,frame_newplayer,frame_profile)
+
+# run main win
+win.mainloop()
